7bbfeebcd034ef048830a84a49fc47ba4b194f93
James Pederson <james@jpederson.com>
2016-02-11 16:12:34 -0600

    A few updates to the styles, scripts, and rate..

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -4,7 +4,7 @@ $(function(){
 
 	$(".rate").keyup(function(){
 		var new_rate = parseFloat( $(this).val().replace(/[^\d.]/ig, '') ) - 1;
-		$(".rate_compare").val( ( new_rate < 1.75 ? 1.75 : new_rate ) );
+		$(".rate_compare").val( ( new_rate < 1.99 ? 1.99 : new_rate ) );
 	});
 
 	$(".calculator").accrue({
